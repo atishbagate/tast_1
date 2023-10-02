9003ef79a6d460376fa12a9ceca49ba21d0e8bfd
Atish <atishbagate@gmail.com>
2023-10-03 00:04:09 +0530

    api endpoint added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Counter from "./Pages/Counter";
 import Home from "./Pages/Home/Home";
+import ErrorPage from "./Pages/ErrorPage/ErrorPage";
 
  
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+     <Route path="*" element = {<ErrorPage/>} />
     <Route path="/" element={<Counter />} /> 
     <Route path="/home" element={<Home/>} />
     </>
-  )
+  ) 
 )
--- src/slices/CounterSlice.ts
+++ src/slices/CounterSlice.ts
@@ -1,6 +1,25 @@
-import { Slice,createSlice } from "@reduxjs/toolkit";
+import { Slice,createAsyncThunk,createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from "../../store";
+import axios from "axios";
+import API_Endpoints from "../utils/API_Endpoints";
+
+
+export const fetchPOSTS = createAsyncThunk<unknown | null , {value : number}>(
+    'counter/fetchPOSTS',
+    async ({value=0},thunkAPI) => {
+        const dispatch = thunkAPI.dispatch;
+        const state : RootState = await thunkAPI.getState();
+        const res: any = await axios.get(API_Endpoints.GET_POST)
+
+        if(res != null){
+            console.log("List ",res);
+            if (res.data){
+                return res.data as unknown;
+            }
+        }
+    }
+)
 
 interface ICounter {
 count:number,
